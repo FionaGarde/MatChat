a8806486deb1b186544ddc5edac8c7ff5809d835
gaelleDijon <gaelledijon@gmail.com>
2023-04-10 11:22:11 +0200

    search maj

--- mobile/lib/controller/search_personne.dart
+++ mobile/lib/controller/search_personne.dart
@@ -3,6 +3,7 @@ import 'package:matchat/controller/FirestoreHepler.dart';
 import 'package:matchat/globale.dart';
 import 'package:matchat/model/utilisateur.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 import 'FirestoreHepler.dart';
 
@@ -38,41 +39,50 @@ class _SearchPersonnState extends State<SearchPersonn> {
         builder: (context, snap) {
           List documents = snap.data?.docs ?? [];
           if (documents.isEmpty) {
-            return const Center(
-              child: Text('Aucun contact trouvé'),
-            );
+            return const Center(child: Text('Aucun contact trouvé'));
           } else {
+            List<String> contactPseudos = [];
+            for (var doc in documents) {
+              String pseudo1 = doc['pseudo1'];
+              String pseudo2 = doc['pseudo2'];
+              if (pseudo1 == monUtilisateur.pseudo) {
+                contactPseudos.add(pseudo2);
+              } else if (pseudo2 == monUtilisateur.pseudo) {
+                contactPseudos.add(pseudo1);
+              }
+            }
             return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (context, index) {
-                Map<String, dynamic> data = documents[index].data();
-                String contactPseudo = data['pseudo1'];
-                return FutureBuilder<DocumentSnapshot>(
-                  future: FirestoreHelper().cloudUsers.doc(contactPseudo).get(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      Utilisateur otherUser = Utilisateur(snapshot.data!);
-                      return Card(
-                        elevation: 5,
-                        child: ListTile(
-                          onTap: () {
-                            //ouvrir une nouvelle page de chat todo
-                          },
-                          leading: CircleAvatar(
-                            radius: 30,
-                            backgroundImage: NetworkImage(otherUser.avatar!),
-                          ),
-                          title: Text(otherUser.fullName),
-                          subtitle: Text(otherUser.pseudo),
-                        ),
-                      );
-                    } else {
-                      return Container();
-                    }
-                  },
-                );
-              },
-            );
+                itemCount: contactPseudos.length,
+                itemBuilder: (context, index) {
+                  return FutureBuilder<DocumentSnapshot>(
+                      future: FirestoreHelper()
+                          .cloudUsers
+                          .doc(contactPseudos[index])
+                          .get(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CircularProgressIndicator.adaptive();
+                        } else {
+                          Utilisateur otherUser = Utilisateur(snapshot.data!);
+                          return Card(
+                            elevation: 5,
+                            child: ListTile(
+                              onTap: () {
+                                //ouvrir une nouvelle page de chat todo
+                              },
+                              leading: CircleAvatar(
+                                radius: 30,
+                                backgroundImage:
+                                    NetworkImage(otherUser.avatar!),
+                              ),
+                              title: Text(otherUser.fullName),
+                              subtitle: Text(otherUser.mail),
+                            ),
+                          );
+                        }
+                      });
+                });
           }
         },
       ),
